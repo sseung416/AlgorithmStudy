3f333142797e7c3eb3ebcfcdc88b53728da2640b
sseung416 <choiseungyeon416@gmail.com>
2022-10-19 20:17:36 +0900

    Solve Baekjoon 1991

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import sseung.tree.Sseung_Baekjoon_1991;
 import tree.BinaryTree;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree.run();
+        // BinaryTree.run();
+        Sseung_Baekjoon_1991.run();
     }
 }
