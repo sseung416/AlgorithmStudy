cda2be12d86d92364506f60028fd7620653f5ec8
sseung416 <choiseungyeon416@gmail.com>
2023-07-11 15:31:26 +0900

    Solve: 백준 N과 M(5) 문제 풀이

---
+++ src/codingtest/Baekjoon_15654.java
@@ -0,0 +1,51 @@
+package codingtest;
+
+import java.util.*;
+import java.io.*;
+
+// N과 M (5): https://www.acmicpc.net/problem/15654
+public class Baekjoon_15654 {
+
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+    static int[] values, result;
+    static boolean[] checked;
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            values = new int[n];
+            result = new int[m];
+            checked = new boolean[n];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                values[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(values);
+            br.close();
+            dfs(0);
+            System.out.print(sb.toString());
+        } catch (IOException ignore) {}
+    }
+
+    public static void dfs(int index) {
+        if (index == m) {
+            for (int value : result) {
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!checked[i]) {
+                checked[i] = true;
+                result[index] = values[i];
+                dfs(index + 1);
+                checked[i] = false;
+            }
+        }
+    }
+}
