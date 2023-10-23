b1ca36159ea8e473c1fcf737215d33af253630c5
sseung416 <choiseungyeon416@gmail.com>
2023-10-23 11:30:43 +0900

    Solve: Leetcode Two Sum 문제 풀이

---
+++ src/codingtest/Leetcode_1.java
@@ -0,0 +1,18 @@
+package codingtest;
+
+import java.util.*;
+
+// Two Sum: https://leetcode.com/problems/two-sum/
+public class Leetcode_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value, index
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return null;
+    }
+}
