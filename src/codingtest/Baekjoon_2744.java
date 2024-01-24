59b42c91d15ccdc13fc1323d0cbce38dc73b65ae
sseung416 <choiseungyeon416@gmail.com>
2024-01-24 18:13:51 +0900

    Solve: 백준 대소문자 바꾸기 문제 풀이

---
+++ src/codingtest/Baekjoon_2744.java
@@ -0,0 +1,21 @@
+package codingtest;
+
+import java.io.*;
+
+// 대소문자 바꾸기: https://www.acmicpc.net/problem/2744
+// 문자열, 구현
+public class Baekjoon_2744 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (char c : br.readLine().toCharArray()) {
+            if (c < 97) {
+                sb.append(Character.toString(c + 32));
+            } else {
+                sb.append(Character.toString(c - 32));
+            }
+        }
+        br.close();
+        System.out.print(sb);
+    }
+}
