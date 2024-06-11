4f7e0baf8b7931e0ba89af4afbdc5afa8d7a0f16
sseung416 <choiseungyeon416@gmail.com>
2024-06-11 22:36:47 +0900

    Solve: 백준 파일 정리 문제 풀이

---
+++ src/codingtest/Baekjoon_20291.java
@@ -0,0 +1,31 @@
+package codingtest;
+
+import java.util.*;
+import java.io.*;
+
+// 파일 정리: https://www.acmicpc.net/problem/20291
+// 자료구조, 문자, 정렬
+// split() 사용시 정규식 체크 유의하기
+// Set to List 형변환 방법 알기
+public class Baekjoon_20291 {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> map = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            String[] split = line.split("\\.");
+            int count = map.getOrDefault(split[1], 0) + 1;
+            map.put(split[1], count);
+        }
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            sb.append(key).append(' ').append(map.get(key)).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
